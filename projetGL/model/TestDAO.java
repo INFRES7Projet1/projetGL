b00fb55791ae5b8a245232491d6c19c0ccc99fd9
Artheis <aymeric.cossard@gmail.com>
2015-06-14 01:43:01 +0200

    Connection à la BDD
    Fichier de connexion à la BDD + Fichier de test

---
+++ projetGL/model/TestDAO.java
@@ -0,0 +1,13 @@
+
+public class TestDAO {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		EscrimDAO database = new EscrimDAO();
+		
+		System.out.println(database.GetAllColis());
+		
+		
+	}
+
+}
